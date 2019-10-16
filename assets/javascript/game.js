e1e9f249ce235a20fabcf627f0838e6c80828517
Farrah Carter <carterfarraha@gmail.com>
2019-10-16 14:45:04 -0400

    Added lower case converse, alphabet array, letter restriction

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,36 @@
-document.onkeyup = function() {
+let gameBegin = false;
+let winCount = 0;
+
+document.onkeyup = function(event) {
     document.getElementById('gameText').style.display = "inline-block";
     document.getElementById('initialText').style.display = "none";
+    gameBegin= true;
+        
+    const correctLetters = document.getElementById('correctLetters');
+    const totalWins = document.getElementById('totalWins');
+    
+    document.onkeyup = function(event) {
+    if (gameBegin == true){
+
+            let userInput = event.key.toLowerCase();
+            const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+            
+            if (alphabet.includes(userInput) === true){
+                correctLetters.textContent = userInput;
+                winCount++;
+                totalWins.textContent = winCount;
+            } else {
+                alert("Not a letter!")
+            }
+
+
+            
+            
+
+    }
+
+
+
+
+    }
  }
