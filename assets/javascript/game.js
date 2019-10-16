eb6c110a7e41c9d77a81e0cc55594c33218b75a0
Farrah Carter <carterfarraha@gmail.com>
2019-10-16 15:41:34 -0400

    Logs all letter presses, displays in window

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,36 +1,53 @@
 let gameBegin = false;
 let winCount = 0;
+let correctSet = [];
 
-document.onkeyup = function(event) {
-    document.getElementById('gameText').style.display = "inline-block";
-    document.getElementById('initialText').style.display = "none";
-    gameBegin= true;
-        
-    const correctLetters = document.getElementById('correctLetters');
-    const totalWins = document.getElementById('totalWins');
-    
-    document.onkeyup = function(event) {
-    if (gameBegin == true){
+document.onkeyup = function (event) {
+        document.getElementById('gameText').style.display = "inline-block";
+        document.getElementById('initialText').style.display = "none";
+        gameBegin = true;
 
-            let userInput = event.key.toLowerCase();
-            const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-            
-            if (alphabet.includes(userInput) === true){
-                correctLetters.textContent = userInput;
-                winCount++;
-                totalWins.textContent = winCount;
-            } else {
-                alert("Not a letter!")
-            }
+        const correctLetters = document.getElementById('correctLetters');
+        const totalWins = document.getElementById('totalWins');
+
+        document.onkeyup = function (event) {
+               
+                if (gameBegin == true) {
+
+                        let userInput = event.key.toLowerCase();
+                        const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+                        let userGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+                        
+                        const wrongSet = [""];
+
+                        //Check for alphabet input
+                        if (alphabet.includes(userInput) === true) {
+                                
+                                correctSet.push(userInput);
+
+                                //Print Correct Guesses
+                                correctLetters.textContent = "";
+                                for (i = 0; i < correctSet.length; i++) {
+                                        let text = correctSet[i];
+                                        correctLetters.append(text + " ");
+                                }
+
+                                winCount++;
+                                totalWins.textContent = winCount;
+
+                        } else {
+                                alert("Not a letter!")
+                        }
 
 
-            
-            
-
-    }
 
 
 
 
-    }
- }
+                }
+
+
+
+
+        }
+}
