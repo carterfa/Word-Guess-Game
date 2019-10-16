14d2639fabbd4d753e868c916826fce891823d9e
Farrah Carter <carterfarraha@gmail.com>
2019-10-16 16:06:01 -0400

    Removed extra document.key, changed header text to mystery word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,53 +1,48 @@
-let gameBegin = false;
+
 let winCount = 0;
-let correctSet = [];
+let wordBank = ["shark", "bear", "eagle", "rooster",]
+
+const correctLetters = document.getElementById('correctLetters');
+const mysteryWord = document.getElementById('mysteryWord');
+const totalWins = document.getElementById('totalWins');
+
+let correctSet = [""];
+let wrongSet = [""];
+
 
 document.onkeyup = function (event) {
-        document.getElementById('gameText').style.display = "inline-block";
-        document.getElementById('initialText').style.display = "none";
-        gameBegin = true;
 
-        const correctLetters = document.getElementById('correctLetters');
-        const totalWins = document.getElementById('totalWins');
-
-        document.onkeyup = function (event) {
-               
-                if (gameBegin == true) {
-
-                        let userInput = event.key.toLowerCase();
-                        const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-                        let userGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-                        
-                        const wrongSet = [""];
-
-                        //Check for alphabet input
-                        if (alphabet.includes(userInput) === true) {
-                                
-                                correctSet.push(userInput);
-
-                                //Print Correct Guesses
-                                correctLetters.textContent = "";
-                                for (i = 0; i < correctSet.length; i++) {
-                                        let text = correctSet[i];
-                                        correctLetters.append(text + " ");
-                                }
-
-                                winCount++;
-                                totalWins.textContent = winCount;
-
-                        } else {
-                                alert("Not a letter!")
-                        }
+        mysteryWord.textContent = wordBank[0];
+        let userInput = event.key.toLowerCase();
+        const alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        let userGuesses = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 
 
+        //Check for alphabet input
+        if (alphabet.includes(userInput) === true) {
 
 
+                correctSet.push(userInput);
 
+                //Print Correct Guesses
+                correctLetters.textContent = "";
+                for (i = 0; i < correctSet.length; i++) {
+                        let text = correctSet[i];
+                        correctLetters.append(text + " ");
                 }
 
+                //winCount++;
+                //totalWins.textContent = winCount;
 
-
-
+        } else {
+                alert("Not a letter!")
         }
+
 }
+
+
+
+
+
+
