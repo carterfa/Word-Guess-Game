b607df1c548853910ec0af6539a9b2ead08adda7
Farrah Carter <carterfarraha@gmail.com>
2019-10-25 23:49:28 -0400

    Added display function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
+//establishes global variables
+let guessedSet = [];
+let mysterySet = [];
 let winCount = 0;
-let wordBank = ["spider", "shark", "bear", "eagle", "rooster", "rabbit", "dog", "crab", "elephant", "tiger", "sea turtle", "kangaroo", "giant panda", "weasel", "whale", "dolphin", "giraffe", "rhino", "gorilla", "zebra"]
 let gameState = true;
+let guessNum = 10;
 
 const guessedLetters = document.getElementById('guessedLetters');
 const mysteryWordText = document.getElementById('mysteryWordText');
@@ -10,16 +13,11 @@ const resetBtn = document.getElementById("resetBtn");
 const message = document.getElementById("message");
 const gameText = document.getElementById("gameText");
 
-//establishes empty sets
-let guessedSet = [];
-let mysterySet = [];
-
-//establishes guesses
-let guessNum = 10;
-
 const game = {
 
+        //generates new word and prepares the game for play
         reset: function () {
+                let wordBank = ["spider", "shark", "bear", "eagle", "rooster", "rabbit", "dog", "crab", "elephant", "tiger", "sea turtle", "kangaroo", "giant panda", "weasel", "whale", "dolphin", "giraffe", "rhino", "gorilla", "zebra"]
                 //reset sets
                 guessedSet = [];
                 mysterySet = [];
@@ -30,11 +28,11 @@ const game = {
                 gameState = true;
 
                 //reset display
+                gameText.style.display = "block";
                 guessedLetters.textContent = "";
                 guessTxt.textContent = "10";
 
                 //Generates random mystery word from word bank
-                mysteryWordText.textContent = "";
                 mysteryWord = wordBank[Math.floor(Math.random() * wordBank.length)];
                 console.log(mysteryWord);
 
@@ -47,23 +45,32 @@ const game = {
                         }
                 }
 
-                //Displays contents of dash array
-                for (i = 0; i < mysterySet.length; i++) {
-                        if (mysterySet[i] === " ") {
-                                mysteryWordText.append('\xa0');
-                        } else {
-                                let character = mysterySet[i];
-                                mysteryWordText.append(character + " ");
-                                console.log(character);
-                        }
-
-                }
+                game.display();
 
                 resetBtn.style.display = "none";
                 message.textContent = "";
 
         },
 
+        //displays the contents of the mystery array
+        display: function () {
+
+                //Displays contents of array
+                mysteryWordText.textContent = "";
+                for (i = 0; i < mysterySet.length; i++) {
+                        let character = mysterySet[i];
+                        if (mysterySet[i] === " ") {
+                                //displays space in case of animal name with 2 words
+                                mysteryWordText.append('\xa0');
+                        } else {
+                                mysteryWordText.append(character.toUpperCase() + " ");
+                        }
+
+                }
+
+        },
+
+        //checks to see if player won and if so displays dialog
         win: function () {
                 if (!mysterySet.includes("_")) {
                         winCount++;
@@ -71,114 +78,91 @@ const game = {
                         message.textContent = "YOU WIN!";
                         resetBtn.style.display = "block";
                         gameState = false;
-
-                        //game.reset();
                 }
 
         },
 
+        //checks to see if player lost and if so displays dialog
         over: function () {
-              if (gameState ==true) {
+                if (guessNum <= 0 && gameState === true) {
+                        mysteryWordText.textContent = mysteryWord.toUpperCase();
+                        message.textContent = "GAME OVER!";
+                        resetBtn.style.display = "block";
+                        gameState = false;
+                }
 
-                mysteryWordText.textContent = mysteryWord.toUpperCase();
-                message.textContent = "GAME OVER!";
-                resetBtn.style.display = "block";
-                gameState = false;
-              }
-                //game.reset();
+        },
+
+        //adds guessed letters
+        print: function (userInput) {
+
+                guessedSet.push(userInput);
+
+                //Print Guess
+                guessedLetters.textContent = "";
+                for (i = 0; i < guessedSet.length; i++) {
+                        let text = guessedSet[i].toUpperCase();
+                        guessedLetters.append(text + " ");
+
+                }
+
+
+        },
+
+        //reviews user input to determine if wrong or correct guess
+        check: function (userInput) {
+                //Searches mystery word for letters incl. repeats, if found letter adjust mystery set array
+                let letterCheck = false;
+                for (i = 0; i <= mysteryWord.length; i++) {
+                        if (userInput == mysteryWord[i]) {
+                                letterCheck = true;
+                                mysterySet[i] = userInput;
+                                //console.log(mysterySet);
+                        }
+                }
+
+                //for when no match was found
+                if (!letterCheck) {
+
+                        game.print(userInput);
+                        game.over();
+
+                        //for when matches were found
+                } else {
+                        // //Redisplay to include found letters
+                        // mysteryWordText.textContent = "";
+                        // for (i = 0; i < mysterySet.length; i++) {
+                        //         let character = mysterySet[i];
+                        //         if (mysterySet[i] === " ") {
+                        //                 mysteryWordText.append('\xa0');
+                        //         } else {
+                        //                 mysteryWordText.append(character.toUpperCase() + " ");
+                        //         }
+                        // }
+                        game.display();
+                        game.print(userInput);
+                        game.win();
+                        game.over();
+                }
+
         }
-
-
 }
 
-document.onkeyup = function (event) {
-
-        game.reset();
-        gameText.style.display = "block";
-
+document.addEventListener("DOMContentLoaded", function () {
 
         //user interaction function
         document.onkeyup = function (event) {
 
                 let userInput = event.key.toLowerCase();
 
-
                 //Check for alphabet input
                 if (userInput.match(/^[a-z]$/) && !guessedSet.includes(userInput) && gameState === true) {
+
                         guessNum--;
-                        
-                        //Searches mystery word for letters incl. repeats, if found letter adjust mystery set array
-                        let letterCheck = false;
-                        for (i = 0; i <= mysteryWord.length; i++) {
-                                if (userInput == mysteryWord[i]) {
-                                        letterCheck = true;
-                                        mysterySet[i] = userInput;
-                                        //console.log(mysterySet);
-                                }
+                        guessTxt.textContent = guessNum;
 
-                        }
-
-                        //let letterCheck = mysteryWord.search(userInput);
-                        //console.log(letterCheck)
-
-                        if (!letterCheck) {
-                                guessedSet.push(userInput);
-                                //Print Wrong Guess
-                                guessedLetters.textContent = "";
-                                for (i = 0; i < guessedSet.length; i++) {
-                                        let text = guessedSet[i].toUpperCase();
-                                        guessedLetters.append(text + " ");
-
-                                }
-
-                                if (guessNum <= 0) {
-                                        game.over();                        
-                                }
-
-                        } else {
-                                //Correct Guess
-                                guessedSet.push(userInput);
-
-                                //Redisplay to include found letters
-                                mysteryWordText.textContent = "";
-                                for (i = 0; i < mysterySet.length; i++) {
-                                        let character = mysterySet[i];
-                                        if (mysterySet[i] === " ") {
-                                                mysteryWordText.append('\xa0');
-                                        } else {
-                                                mysteryWordText.append(character.toUpperCase() + " ");
-                                        }
-                                }
-
-                                //Print Correct Guesses
-                                guessedLetters.textContent = "";
-                                for (i = 0; i < guessedSet.length; i++) {
-                                        let text = guessedSet[i].toUpperCase();
-                                        guessedLetters.append(text + " ");
-                                }
-
-                                game.win ();
-
-                                if (guessNum <= 0) {
-                                        game.over();                        
-                                }
-
-                        }
-
-                        if (guessNum >= 0) {
-                                guessTxt.textContent = guessNum;
-
-                        }
-
-                        console.log(guessNum)
-
-                        //check for win state
-
-
-
-
+                        game.check(userInput);
                 }
         }
-
-}
+});
 
